98d81a4aeeb7e8cd15a2dde52500f07d0b5a941a
rodrigojgq <rodrigojgq@gmail.com>
2021-12-13 16:51:22 -0400

    Finish the use of context in components

--- src/App/AppUI.js
+++ src/App/AppUI.js
@@ -7,39 +7,36 @@ import { ListItem } from '../ListItem';
 import { CreateItemButton } from '../CreateItemButton';
 
 function AppUI() {
+  const {
+    error,
+    loading,
+    searchedItems,
+    toggleBuyItem,
+    deleteItem,
+  } = React.useContext(ItemContext);
   return (
   <React.Fragment>
     <ListCounter />
     <ListSearch />
 
-    <ItemContext.Consumer>
-      {({
-        error,
-        loading,
-        searchedItems,
-        toggleBuyItem,
-        deleteItem,
-      }) => (
-        <BuyingList>
-          {error && <p>Hubo un error...</p>}
-          {loading && <p>Cargando la lista de compras...</p>}
-          {(!loading && !searchedItems.length) && <p>Crea el primer item para comprar</p>}
+    <BuyingList>
+      {error && <p>Hubo un error...</p>}
+      {loading && <p>Cargando la lista de compras...</p>}
+      {(!loading && !searchedItems.length) && <p>Crea el primer item para comprar</p>}
 
-          {searchedItems.map((item) => (
-            <ListItem
-              key = {item.name}
-              text = {item.name}
-              quantity = {item.quantity}
-              measure = {item.measure}
-              buyed = {item.buyed}
-              toggleBuyed = {() => toggleBuyItem(item.name)}
-              onDelete = {() => deleteItem(item.name)}
-            />
-          ))}
-        </BuyingList>
-      )
-      }
-    </ItemContext.Consumer>
+      {searchedItems.map((item) => (
+        <ListItem
+          key = {item.name}
+          text = {item.name}
+          quantity = {item.quantity}
+          measure = {item.measure}
+          buyed = {item.buyed}
+          toggleBuyed = {() => toggleBuyItem(item.name)}
+          onDelete = {() => deleteItem(item.name)}
+        />
+      ))}
+    </BuyingList>
+    
     <CreateItemButton />
   </React.Fragment>
   );
--- src/ListCounter/index.js
+++ src/ListCounter/index.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { ItemContext } from "../ItemContext";
 import './ListCounter.css';
 
-function ListCounter({total, buyed}) {
+function ListCounter() {
+  const {totalItems, buyedItems} = React.useContext(ItemContext);
+
   return (
     <>
       <h1>Lista de Compras</h1>
-      <h2>Has comprado {buyed} de {total} items de tu lista</h2>
+      <h2>Has comprado {buyedItems} de {totalItems} items de tu lista</h2>
     </>
   );
 
