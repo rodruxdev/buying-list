4f5f61c31e64533d21d815153e607220c413d82f
rodrigojgq <rodrigojgq@gmail.com>
2022-01-10 15:24:25 -0400

    Finish styles for notification

--- src/ChangeAlert/index.js
+++ src/ChangeAlert/index.js
@@ -1,17 +1,21 @@
 import React from "react";
 import { useStorageListener } from "./useStorageListener";
+import "./ChangeAlert.css";
 
 function ChangeAlert({ sincronize }) {
   const { show, toggleShow } = useStorageListener(sincronize);
   if(show){
     return (
-      <div>
-        <p>Hubo cambios en los items</p>
-        <button
-          onClick={() => toggleShow()}
-        >
-          volver a cargar la información
-        </button>
+      <div className="bkg-notification">
+        <div className="notification--container">
+          <p className="notification--message">Hubo cambios en los items en otra pestaña.</p>
+          <button
+            onClick={() => toggleShow()}
+            className="notification--button"
+          >
+            VOLVER A CARGAR
+          </button>
+        </div>
       </div>
     )
   } else {
